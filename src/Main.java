1b01fbe3b9fe94ae10d110ad79cfa5eaba25b34e
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-12 20:46:19 -0500

    test

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import code.model.Product;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("helloWorld "+ "!");
+        Product product = new Product( "Keyboard");
+        System.out.println(product.getName());
+    }
+}
---
+++ src/code/model/Product.java
@@ -0,0 +1,13 @@
+package code.model;
+
+public class Product {
+     String name;
+    public Product(String name){
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
